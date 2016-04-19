d7b18f93bda44e465e418bb21dd1c57e111a7dc6
aldolyna <aldolyna@gmail.com>
2016-04-19 20:53:09 +0300

    aldolyna: test commit

--- src/com/trojanhan/Main.java
+++ src/com/trojanhan/Main.java
@@ -1,8 +1,12 @@
 package com.trojanhan;
 
+/**
+ * Main class for learning Java
+ */
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print(1);
+        // "Hello world" message to defalt system out
+        System.out.print("Hello world");
     }
 }
