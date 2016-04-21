370a264ccc8fdaaf71abb927b76775eeeda09b9b
aldolyna <aldolyna@gmail.com>
2016-04-21 19:44:37 +0300

    aldolyna: test commit

--- src/com/trojanhan/Main.java
+++ src/com/trojanhan/Main.java
@@ -3,7 +3,6 @@ package com.trojanhan;
 public class Main {
 
     public static void main(String[] args) {
-        // To dolyna
-        System.out.print(2);
+        System.out.print("Hello Andrey!");
     }
 }
